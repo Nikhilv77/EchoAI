4bc7c552dd6646881185174abb155a9dfd474aae
Nikhil <nv581339@gmail.com>
2024-01-15 18:08:12 +0530

    Fix warnings and errors in build

--- components/Q&A/QuestionAndAnswer.js
+++ components/Q&A/QuestionAndAnswer.js
@@ -1,6 +1,7 @@
 import { useUser } from "@auth0/nextjs-auth0/client";
 import ReactMarkdown from "react-markdown";
 import React from "react";
+import Image from "next/image";
 
 export const QuestionAndAnswer = React.memo(({ type, content }) => {
   const { user } = useUser();
@@ -10,13 +11,13 @@ export const QuestionAndAnswer = React.memo(({ type, content }) => {
       <div className="QorA">
         {type === 'question' && user && (
           <div className="question">
-            <img src={user.picture} alt="" />
+            <Image src={user.picture} alt="" />
             <span>{user.name}</span>
           </div>
         )}
         {type === 'answer' && (
           <div className="answer">
-            <img src='/favicon.png' alt="" />
+            <Image src='/favicon.png' alt="" />
             <span>EchoAI</span>
           </div>
         )}
